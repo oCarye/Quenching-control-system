8f1d08c1c2321f9782a0c7a29c3930f94ae16524
oCarye <oCarye2000@163.com>
2021-04-03 19:18:46 +0800

    画出有两个子图的表格

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
-import src
+from src import data
+from src import graph
 x1,x2,y1,y2=data.getData("res\QT1_1210-15.csv")
-graph.drawGraph(x1,y1)
+graph.drawGraph(x1,y1,x2,y2)
--- src/graph.py
+++ src/graph.py
@@ -1,4 +1,16 @@
 import matplotlib.pyplot as plt
-def drawGraph(x,y):
-    plt.bar(x,y)
-    plt.show()
+from modules.config import *
+def drawGraph(x1,y1,x2,y2):
+    plt.figure(figsize=(10,8))  #画布大小
+    plt.subplot(2,1,1)  #创建2*1的区域内的第一个子图
+    plt.bar(x1,y1)  #绘制条状图
+    plt.xlabel(sp_time_1_3)  #横坐标名称
+    plt.ylabel(sp_value_1_3)
+    plt.xticks(range(0,len(x1),20))  #横坐标间距
+
+    plt.subplot(2,1,2)
+    plt.bar(x2,y2)
+    plt.xlabel(av_time_1_3)
+    plt.ylabel(av_value_1_3)
+    plt.xticks(range(0,len(x2),20))
+    plt.show()  #显示图像
